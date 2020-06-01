9d72beaa70848d407a80df6d82674d36037051a6
laravelstudy <ryosukemiyazawa+laravelstudy@gmail.com>
2020-06-02 01:58:44 +0900

    Landing PageのHTMLを組み込み

--- company_site/resources/views/parts/top/section04.blade.php
+++ company_site/resources/views/parts/top/section04.blade.php
@@ -1,12 +1,10 @@
-@include("parts.nav")
-
-<!-- Masthead -->
-<header class="masthead text-white text-center">
+<!-- Call to Action -->
+<section class="call-to-action text-white text-center">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5">株式会社○○のサイトにようこそ</h1>
+          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
           <form>
@@ -22,4 +20,4 @@
         </div>
       </div>
     </div>
-  </header>
+  </section>
