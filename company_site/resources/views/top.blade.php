3031563d6bb65e4acfdcc4e445d1e902b5a90caa
miyazawa <ryosukemiyazawa@gmail.com>
2020-06-01 16:32:12 +0900

    トップページの作成1

---
+++ company_site/resources/views/top.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<header>
+	<h1>Company Site</h1>
+	<nav>
+		<a href="/">トップ</a>
+	</nav>
+</header>
+
+<h1>これはトップページです</h1>
+
+<footer>
+	&copyright; 2020 XXXX
+</footer>
+
+</body>
+</html>
